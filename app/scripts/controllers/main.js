8769dce5c3ad9763257401346b88608431dcba5d
Edoardo Spadoni <edoardo.spadoni@gmail.com>
2017-04-12 19:03:18 +0200

    added Fullscreen element

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,8 @@ angular.module('periodicTableApp')
   .controller('MainCtrl', function ($scope) {
     $scope.elements = data.elements;
     $scope.currentElement = {};
+    $scope.isModalOpen = false;
+    $scope.inFullScreen = false;
 
     var screenWidth = window.innerWidth >= 1200 ? window.innerWidth : 1200;
     var screenHeight = window.innerHeight;
@@ -20,6 +22,18 @@ angular.module('periodicTableApp')
 
     $scope.tableWidth = screenWidth;
     $scope.tableHeight = screenHeight;
+
+    $scope.elements.push({
+      "name": "Fullscreen",
+      "category": null,
+      "number": 0,
+      "symbol": "Fu",
+      "xpos": 6,
+      "ypos": 2,
+      "action": function () {
+        $scope.goToFullScreen();
+      }
+    });
 
     $scope.getTimes = function (n) {
       return new Array(n);
@@ -34,6 +48,7 @@ angular.module('periodicTableApp')
     };
 
     $scope.openDetailModal = function (e) {
+      $scope.isModalOpen = true;
       $scope.currentElement = e;
       // Get the modal
       var modal = document.getElementById('elemModal');
@@ -46,12 +61,45 @@ angular.module('periodicTableApp')
       // When the user clicks on <span> (x), close the modal
       span.onclick = function () {
         modal.style.display = "none";
+        $scope.isModalOpen = false;
+        $scope.$apply();
       }
 
       // When the user clicks anywhere outside of the modal, close it
       window.onclick = function (event) {
         if (event.target == modal) {
           modal.style.display = "none";
+          $scope.isModalOpen = false;
+          $scope.$apply();
+        }
+      }
+    };
+
+    $scope.goToFullScreen = function () {
+      if ($scope.inFullScreen) {
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+          $scope.inFullScreen = false;
+        } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+          $scope.inFullScreen = false;
+        } else if (document.webkitExitFullscreen) {
+          document.webkitExitFullscreen();
+          $scope.inFullScreen = false;
+        }
+      } else {
+        if (document.documentElement.requestFullscreen) {
+          document.documentElement.requestFullscreen();
+          $scope.inFullScreen = true;
+        } else if (document.documentElement.mozRequestFullScreen) {
+          document.documentElement.mozRequestFullScreen();
+          $scope.inFullScreen = true;
+        } else if (document.documentElement.webkitRequestFullscreen) {
+          document.documentElement.webkitRequestFullscreen();
+          $scope.inFullScreen = true;
+        } else if (document.documentElement.msRequestFullscreen) {
+          document.documentElement.msRequestFullscreen();
+          $scope.inFullScreen = true;
         }
       }
     };
